ecf5abc9dcefd266e77f07c0c39250c0af5e488c
truizdiaz <tomiruizdiaz@gmail.com>
2020-07-10 16:23:53 -0300

    Creacion Servicio

--- src/app/components/list-alumnos/list-alumnos.component.ts
+++ src/app/components/list-alumnos/list-alumnos.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AlumnoService } from '../../services/alumno.service';
+import { Alumno } from 'src/app/models/alumno';
 @Component({
   selector: 'app-list-alumnos',
   templateUrl: './list-alumnos.component.html',
   styleUrls: ['./list-alumnos.component.css']
 })
 export class ListAlumnosComponent implements OnInit {
+  listAlumnos: Alumno[] = [];
 
-  constructor() { }
+  constructor(private alumnoService: AlumnoService) { }
 
   ngOnInit(): void {
+    this.getAlumno();
+  }
+
+  getAlumno(): void {
+    this.listAlumnos = this.alumnoService.listAlumnos;
   }
 
 }
---
+++ src/app/services/alumno.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Alumno } from '../models/alumno';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlumnoService {
+  listAlumnos: Alumno[] = [
+    { nombre: 'Juan Perez', estado: 'Aprobado' },
+    { nombre: 'Nicolas Garcia', estado: 'No Aprobado' },
+    { nombre: 'Maria Funes', estado: 'Promocionado' }
+  ];
+  constructor() { }
+}
